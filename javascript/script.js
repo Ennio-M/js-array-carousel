83941e01f638e24fe4474ed4336364b84793941d
Ennio-M <marrese.ennio@gmail.com>
2022-04-04 16:55:54 +0200

    milestone 2: markup inserito dinamicamente

--- javascript/script.js
+++ javascript/script.js
@@ -21,3 +21,29 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+let itemTemplate = "";
+let thumbTemplate = "";
+
+let current = 0;
+for(let i = 0; i < items.length; i++) {
+    let active = "";
+    if(i === current) {
+        active = "active";
+    }
+    itemTemplate += `
+    <div class="item ${active}">
+        <img src="./${items[i]}" alt="${title[i]}">
+        <div class="title">
+            <h2>${title[i]}</h2>
+            <p>${text[i]}</p>
+        </div>
+    </div>`;
+    thumbTemplate += `
+    <div class="thumb ${active}">
+        <img src="./${items[i]}" alt="${title[i]}">
+    </div>`
+}
+
+document.querySelector(".items-container").innerHTML = itemTemplate;
+document.querySelector(".thumbs-container").innerHTML += thumbTemplate;

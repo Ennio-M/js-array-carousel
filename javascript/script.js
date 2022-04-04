46b8d5a6698aab1ccea2ea131a2148779e2776d6
Ennio-M <marrese.ennio@gmail.com>
2022-04-04 18:37:26 +0200

    milestone 3: carosello funzionante + bonus

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// Array da visualizzare
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -22,28 +23,71 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
+// Inizializzazione variabili
 let itemTemplate = "";
 let thumbTemplate = "";
-
 let current = 0;
-for(let i = 0; i < items.length; i++) {
-    let active = "";
-    if(i === current) {
-        active = "active";
-    }
-    itemTemplate += `
-    <div class="item ${active}">
-        <img src="./${items[i]}" alt="${title[i]}">
-        <div class="title">
-            <h2>${title[i]}</h2>
-            <p>${text[i]}</p>
-        </div>
-    </div>`;
-    thumbTemplate += `
-    <div class="thumb ${active}">
-        <img src="./${items[i]}" alt="${title[i]}">
-    </div>`
-}
 
-document.querySelector(".items-container").innerHTML = itemTemplate;
-document.querySelector(".thumbs-container").innerHTML += thumbTemplate;
+// Generazione pagina
+generatePage();
+
+// Eventi
+const up = document.querySelector(".fa-circle-chevron-up");
+const down = document.querySelector(".fa-circle-chevron-down");
+up.addEventListener("click", previous);
+down.addEventListener("click", next);
+
+// Funzioni
+// Funzione per generare la pagina
+function generatePage() {
+    for(let i = 0; i < items.length; i++) {
+        let active = "";
+        if(i === current) {
+            active = "active";
+        }
+        itemTemplate += `
+        <div class="item ${active}">
+            <img src="./${items[i]}" alt="${title[i]}">
+            <div class="title">
+                <h2>${title[i]}</h2>
+                <p>${text[i]}</p>
+            </div>
+        </div>`;
+        thumbTemplate += `
+        <div class="thumb ${active}">
+            <img src="./${items[i]}" alt="${title[i]}">
+        </div>`
+    }
+    let itemsContainer = document.querySelector(".items-container");
+    itemsContainer.innerHTML = itemTemplate;
+    let thumbsContainer = document.querySelector(".thumbs-container");
+    thumbsContainer.innerHTML += thumbTemplate;
+}
+// Funzione per mandare indietro le immagini
+function previous() {
+    const imgs = document.getElementsByClassName("item");
+    imgs[current].classList.remove("active");
+    const thumbs = document.getElementsByClassName("thumb");
+    thumbs[current].classList.remove("active");
+    if(current === 0) {
+        current = 4;
+    } else {
+        current--;
+    }
+    imgs[current].classList.add("active");
+    thumbs[current].classList.add("active");
+}
+// Funzione per mandare avanti le immagini
+function next() {
+    const images = document.getElementsByClassName("item");
+    images[current].classList.remove("active");
+    const thumbs = document.getElementsByClassName("thumb");
+    thumbs[current].classList.remove("active");
+    if(current === 4) {
+        current = 0;
+    } else {
+        current++;
+    }
+    images[current].classList.add("active");
+    thumbs[current].classList.add("active");
+}
